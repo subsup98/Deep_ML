518e53ea15fab5851a27c7c4a08cd192379a1f06
limyongsub <dydtjqdla98@kyonggi.ac.kr>
2024-09-09 12:06:56 +0900

    Lv1

---
+++ Matrix_time_Vector(easy).py
@@ -0,0 +1,11 @@
+def matrix_dot_vector(a:list[list[int|float]],b:list[int|float])-> list[int|float]:
+	c=[]
+	if len(a[0]) < len(b):
+		return -1
+	for i in range (len(a)):
+		k=0
+		for j in range(len(b)):
+			k +=a[i][j]*b[j]		
+		c.append(k)
+	
+	return c
