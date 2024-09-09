30237eeeac2be4d32b1c64b5d37a028aea6fa5b2
임용섭_T7235 <96379076+subsup98@users.noreply.github.com>
2024-09-09 12:17:29 +0900

    Update Matrix_time_Vector(easy).py

--- Matrix_time_Vector(easy).py
+++ Matrix_time_Vector(easy).py
@@ -1,6 +1,3 @@
-![image](https://github.com/user-attachments/assets/6e3cf3aa-c223-429a-bb8b-911eee18128e)
-
-
 def matrix_dot_vector(a:list[list[int|float]],b:list[int|float])-> list[int|float]:
 	c=[]
 	if len(a[0]) < len(b):
